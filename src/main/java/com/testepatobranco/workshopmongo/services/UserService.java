6eaa143f80880ce2a7a10c38de227f71bc05f3c1
Robson Cesa <robsoncesa@gmail.com>
2023-01-16 17:39:53 -0300

    inserção de usuário com post

--- src/main/java/com/testepatobranco/workshopmongo/services/UserService.java
+++ src/main/java/com/testepatobranco/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.testepatobranco.workshopmongo.domain.User;
+import com.testepatobranco.workshopmongo.dto.UserDTO;
 import com.testepatobranco.workshopmongo.repository.UserRepository;
 import com.testepatobranco.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,11 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
