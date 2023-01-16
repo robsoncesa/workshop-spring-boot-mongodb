f515c1463a6c6f1f808ab1c15cdcfd9a52f6237d
Robson Cesa <robsoncesa@gmail.com>
2023-01-16 17:09:37 -0300

    obtendo usuário por ID

--- src/main/java/com/testepatobranco/workshopmongo/services/UserService.java
+++ src/main/java/com/testepatobranco/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.testepatobranco.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.testepatobranco.workshopmongo.domain.User;
 import com.testepatobranco.workshopmongo.repository.UserRepository;
+import com.testepatobranco.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
