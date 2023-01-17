70b90f5980dcba5e53c2addf3f6c4ed48013054f
Robson Cesa <robsoncesa@gmail.com>
2023-01-17 11:02:09 -0300

    projeções dados do autor com DTO

--- src/main/java/com/testepatobranco/workshopmongo/config/Instantiation.java
+++ src/main/java/com/testepatobranco/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.testepatobranco.workshopmongo.domain.Post;
 import com.testepatobranco.workshopmongo.domain.User;
+import com.testepatobranco.workshopmongo.dto.AuthorDTO;
 import com.testepatobranco.workshopmongo.repository.PostRepository;
 import com.testepatobranco.workshopmongo.repository.UserRepository;
 
@@ -34,8 +35,8 @@ public class Instantiation implements CommandLineRunner {
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje! . Abraços", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje! . Abraços", new AuthorDTO (maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
